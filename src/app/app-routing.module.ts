57dfc945c247c05ae14cf4cbabc9146f4defcef9
Fabian Castillo <fabianboc960@hotmail.com>
2024-04-06 15:08:26 -0500

    feat(routing): :sparkles: routing
    Ajustar routing de la aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {"path": 'login', component: LoginComponent},
+  {"path": 'register', component: RegisterComponent},
+  {"path": 'dashboard', component: DashboardComponent},
+  {"path": '', redirectTo:'/dashboard', pathMatch:'full'},
+  {"path": '**', component: NopagefoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
