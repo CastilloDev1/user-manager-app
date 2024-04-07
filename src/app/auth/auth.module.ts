bec1f97c2e111548b013a1dc3ffc99eaa4fe68cc
Fabian Castillo <fabianboc960@hotmail.com>
2024-04-07 13:37:39 -0500

    style(register): :art: desing register
    Se realiza plantilla de registro

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -15,7 +18,9 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+declare var $: any;
+declare var jQuery: any;
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: [ './register.component.css' ]
 })
-export class RegisterComponent {
 
+export class RegisterComponent implements OnInit {
+
+  ngOnInit(): void {
+    // Date Picker
+    jQuery('.mydatepicker, #datepicker').datepicker();
+    jQuery('#datepicker-autoclose').datepicker({
+        autoclose: true,
+        todayHighlight: true,
+    });
+    jQuery('#date-range').datepicker({
+        toggleActive: true
+    });
+    jQuery('#datepicker-inline').datepicker({
+        todayHighlight: true
+    });
+    
+  }
+  public formSubmitted: boolean = false;
+  public registerForm = this.fb.group({
+    'name': ['', Validators.required ],
+    'lastname': ['', Validators.required ],
+    'age': ['', Validators.required ],
+    'gender': ['', Validators.required ],
+    'status': false, // optional
+    'birthdate': [''],
+    'phone': ['', Validators.required ],
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ){ }
+
+  crearUsuario() {
+    this.formSubmitted = true;
+    if ( this.registerForm.valid ){
+      console.log(this.registerForm.value);
+    }
+  }
+
+  invalidField( field: string ) {
+    if( this.registerForm.get(field)?.invalid && this.formSubmitted ) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  isNotFemale(){
+    if ( this.registerForm.get('gender')?.value !== 'Female') {
+      this.registerForm.get('status')?.setValue(false)
+      return true;
+    } else {
+      return null;
+    }
+  }
 }
